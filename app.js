2e22ec3142ce030de724837ab1a1eab51725fe7d
Pallab Nath <pallabnath98@gmail.com>
2023-12-01 14:21:48 +0530

    add user functionality

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ mongoose.connect('mongodb://localhost:27017/employee-management')
 app.set('view engine','ejs')
 
 app.use(express.static(publicDir))
+app.use(express.urlencoded({extended:true}))
 
 app.get('/',async(req,res)=>{
     let users = {}
@@ -27,6 +28,17 @@ app.get('/',async(req,res)=>{
 
 app.get('/add_employee',(req,res)=>{
     res.render('addemployee')
+})
+
+app.post('/add_employee',async(req,res)=>{
+    const data = {
+        name:req.body.name,
+        email:req.body.email,
+        gender:req.body.gender,
+        role:req.body.role
+    }
+    await employee.create(data)
+    res.redirect('/')
 })
 
 app.listen(port,()=>{
